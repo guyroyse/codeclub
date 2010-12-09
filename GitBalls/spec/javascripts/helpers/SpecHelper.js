e998604063cb6bb7de712ac74a0283b6a87c8228
unknown <ROYSEG@.nwie.net>
2010-12-09 17:18:34 -0500

    Added some mock up pages, removed Jasmine sample code

--- GitBalls/spec/javascripts/helpers/SpecHelper.js
+++ GitBalls/spec/javascripts/helpers/SpecHelper.js
@@ -1,10 +1,5 @@
 beforeEach(function() {
 	this.addMatchers( {
-		toBePlaying : function(expectedSong) {
-			var player = this.actual;
-			return player.currentlyPlayingSong === expectedSong
-					&& player.isPlaying;
-		},
 		toBeValidColor : function() {
 			var color = this.actual.color;
 			return (color === Ball.RED || color === Ball.GREEN
